a793f1f52393c9905f0d2d3a89b5bedb454624ef
Philippe Blayo <philippe.blayo@gmail.com>
2019-04-18 14:56:17 +0200

    Fix migration to run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_221745) do
+ActiveRecord::Schema.define(version: 2019_04_16_142208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fichiers", force: :cascade do |t|
+    t.string "chemin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fichiers_requests", force: :cascade do |t|
+    t.integer "fichier_id"
+    t.integer "request_id"
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
