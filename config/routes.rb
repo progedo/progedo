64b48539619c26773068a732a17bbcf98ee9c715
cheikht3010 <cheikht2011@gmail.com>
2019-04-23 17:18:37 +0200

    Add fichiers view index and new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :requests
   resources :surveys
+  resources :fichiers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
