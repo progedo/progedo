052c69ef882a8da62ffb7d0cc754c6f1955f1fc9
Philippe Blayo <philippe.blayo@gmail.com>
2019-04-28 23:55:36 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :requests
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
