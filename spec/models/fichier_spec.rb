3a15a91804bc230c3e57585bf374855a81eea3a3
cheikht3010 <cheikht2011@gmail.com>
2019-05-21 11:32:56 +0200

    Addition of spec models for automated tests

--- spec/models/fichier_spec.rb
+++ spec/models/fichier_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Fichier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ context '.create!' do
+
+		it "has one more after adding one" do
+			num = Fichier.count
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			Fichier.create(survey_id: survey.id)
+			expect(Fichier.count).to eq(num + 1)
+		end
+
+		it "must have survey_id" do
+			expect {Fichier.create!}.to  raise_error(ActiveRecord::RecordInvalid)
+		end
+
+		it 'creates fichier with empty fields' do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			fichier = Fichier.create(survey_id: survey.id)
+			expect(fichier.chemin).to be_nil
+		end
+
+		it 'has same survey chemin' do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			fichier = Fichier.create(chemin: "Chemin", survey_id: survey.id)
+			fichier_bis = Fichier.all.last
+			expect(fichier_bis.chemin).to eq fichier.chemin
+		end
+
+	end
+
+	context 'associations' do
+		it { should have_and_belong_to_many :requests }
+		it { should belong_to :survey }
+	end
+
+	context 'attributes' do
+		context 'chemin' do
+			it { should have_db_column(:chemin).of_type(:string).with_options(null: true) }
+		end
+	end
+
+	context "delete fichier" do
+
+		it "loses one after one has been deleted" do
+			num = Fichier.count
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			fichier = Fichier.create(survey_id: survey.id)
+			expect(Fichier.count).to eq(num + 1)
+			fichier.destroy
+			expect(Fichier.count).to eq(num)
+		end
+
+		it "has its survey which persists after its deletion" do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			fichier = Fichier.create(chemin: "first_fichier", survey_id: survey.id)
+			survey.destroy
+			expect(fichier.id).to eq(Fichier.find_by(chemin: "first_fichier").id)
+		end
+
+	end
+
+	context "update fichier" do
+		it "has its chemin changing but not its id" do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			survey2 = Survey.create(title: "Title2", abstract: "Abstract2")
+			fichier = Fichier.create(chemin: "Chemin", survey_id: survey.id)
+			fichier_bis = Fichier.find_by(chemin: "Chemin")
+			fichier.update chemin: "Another Chemin", survey_id: survey2.id
+			expect(fichier_bis.chemin).not_to eq fichier.chemin
+			expect(fichier_bis.survey_id).not_to eq fichier.survey_id
+			expect(fichier_bis.id).to eq fichier.id
+		end
+	end
+
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,5 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe Survey, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+	context '.create!' do
+
+		it "has one more after adding one" do
+			num = Survey.count
+			Survey.create
+			expect(Survey.count).to eq(num + 1)
+		end
+
+		it 'creates survey with empty fields' do
+			survey = Survey.create!
+			expect(survey.title).to be_nil
+			expect(survey.abstract).to be_nil
+		end
+
+		it 'has same survey title' do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			survey_bis = Survey.find_by(abstract: "Abstract")
+			expect(survey_bis.title).to eq survey.title
+		end
+
+		it 'has same survey abstract' do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			survey_bis = Survey.find_by(title: "Title")
+			expect(survey_bis.abstract).to eq survey.abstract
+		end
+
+	end
+  
+
+
+	context 'associations' do
+		it { should have_one(:fichier) }
+	end
+
+	context 'attributes' do
+
+		context 'title' do
+			it { should have_db_column(:title).of_type(:string).with_options(null: true) }
+		end
+
+		context 'abstract' do
+			it { should have_db_column(:abstract).of_type(:text).with_options(null: true) }
+		end
+	end
+
+	context "adding fichier" do
+		it "get one fichier" do
+			survey = Survey.create
+			fichier1 = Fichier.create(chemin: "first fichier")
+			survey.fichier = fichier1
+			expect(survey.fichier).to eq(fichier1)
+		end
+	end
+
+	context "delete survey" do
+
+		it "loses one after one has been deleted" do
+			num = Survey.count
+			survey = Survey.create
+			expect(Survey.count).to eq(num + 1)
+			survey.destroy
+			expect(Survey.count).to eq(num)
+		end
+
+		it "has its file which persists after its deletion" do
+			survey = Survey.create
+			fichier1 = Fichier.create(chemin: "first_fichier")
+			survey.fichier = fichier1
+			survey.destroy
+			expect(fichier1.id).to eq(Fichier.find_by(chemin: "first_fichier").id)
+		end
+
+	end
+
+	context "update survey" do
+		it "has its title or abstract changing but not its id" do
+			survey = Survey.create(title: "Title", abstract: "Abstract")
+			survey_bis = Survey.find_by(abstract: "Abstract")
+			survey.update title: "Another Title", abstract: "Another Abstract"
+			expect(survey_bis.title).not_to eq survey.title
+			expect(survey_bis.abstract).not_to eq survey.abstract
+			expect(survey_bis.id).to eq survey.id
+		end
+	end
 end
